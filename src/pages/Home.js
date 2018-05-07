cddb1f286132d40cfb51803bd07ea033d8e5cb58
iruslani <iruslani75@gmail.com>
2018-05-07 00:31:31 -0700

    Css tweak to home page search.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import SearchBar from '../components/SeachBar'
 const Home = ({location}) => (
     <Container fluid className='landing '>
     <Row className='h-100 justify-content-center align-items-center'>
-        <Col sm="12" md={{ size: 8, offset: 2 }}>
+        <Col sm="12" md={{ size: 8 }}>
             <div className='search-container'>
                 <h1>Find your new home.</h1>
                 <SearchBar />
